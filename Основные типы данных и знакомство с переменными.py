41cf3f0eabda07c3f81385b93727c42064208f10
_No_name_ <150374660+NamelessKing45@users.noreply.github.com>
2024-11-23 00:21:57 +0300

    Create Основные типы данных и знакомство с переменными.py

---
+++ Основные типы данных и знакомство с переменными.py
@@ -0,0 +1,6 @@
+number_completed_hw = 12
+number_hours_spent = 1.5
+name_course = 'Python'
+time_one_ex = number_hours_spent / number_completed_hw
+print(f'Курс: {name_course}, всего задач:{number_completed_hw}, затрачено часов: {number_hours_spent}, '
+f'среднее время выполнения {time_one_ex} часа.')
