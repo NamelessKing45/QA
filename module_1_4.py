a4625fa93d0648cc65bab605b9fc28b22b2c1b31
_No_name_ <150374660+NamelessKing45@users.noreply.github.com>
2024-11-23 04:54:48 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,7 +1,19 @@
 """
-Выведите строку my_string в верхнем регистре.
-Выведите строку my_string в нижнем регистре.
-Измените строку my_string, удалив все пробелы.
-Выведите первый символ строки my_string.
-Выведите последний символ строки my_string.
+1)Выведите строку my_string в верхнем регистре.
+2)Выведите строку my_string в нижнем регистре.
+3)Измените строку my_string, удалив все пробелы.
+4)Выведите первый символ строки my_string.
+5)Выведите последний символ строки my_string.
 """
+my_string = str(input())
+
+#1
+print(my_string.upper())
+#2
+print(my_string.lower())
+#3
+print(my_string.replase(' ',''))
+#4
+print(my_string[0])
+#5
+print(my_string[-1])
