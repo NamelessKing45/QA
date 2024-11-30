a82dedb57d9ba1dc833eeebcfed13f891a370cd1
_No_name_ <150374660+NamelessKing45@users.noreply.github.com>
2024-12-01 02:21:27 +0300

    Create module_2_4_1.py

---
+++ module_2_4_1.py
@@ -0,0 +1,26 @@
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+ch_numbers = 0
+
+
+for num in numbers:
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    while is_prime is True:
+        try:
+            primes.append(num)
+            break
+        except ValueError:
+            pass
+    while is_prime is False:
+        try:
+            not_primes.append(num)
+            break
+        except ValueError:
+            pass
+print(f'{primes}\n{not_primes}')
